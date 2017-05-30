34f902b41511defc06a696531df70a7952aaa3dd
Fisherman33 <kevin.videau@gmail.com>
2017-05-30 13:21:39 +0200

    modif user

--- subject/server/server.js
+++ subject/server/server.js
@@ -2,7 +2,8 @@ let express = require('express')
 var app = express()
 var bodyParser = require('body-parser')  
 var server = app.listen(9000)
-
+/*var data = require("./mocks/users.json") // route pour récuperation du Json
+var users = data.users*/        
 // config
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -11,12 +12,33 @@ app.use(function (req, res, next) {
 });
 app.use(bodyParser.json());
 
-// dossier static
+
 app.use('./', express.static('public'))  
 
-// route
 app.get('/', function (req, res) {
     res.send("Essayer l'URL")
 })
+/*
+app.get('./mocks/users.json', function (req, res) { 
+    res.send(data)
+})
+
+app.get('/users/:id', function (req, res) { 
+    let reqId = req.params.id               // l'endroit pour recuperer l'id
+    var user = getUserById(reqId)
+    console.log(user)
+    res.send(user)
+})
+
 
 console.log('Adresse du serveur: http://localhost:9000');
+
+function getUserById(id) {
+    for (let i = 0; i < users.length; i++) {
+        var user = ninjas[i]
+        if (user._id == id) {
+            return user
+        }
+        console.log("user : ", user)
+    }
+}*/
